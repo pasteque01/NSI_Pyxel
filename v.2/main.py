2a363c877d90ed631a82fa727455a12f7216b4f5
Julian <128595790+pasteque01@users.noreply.github.com>
2023-05-06 19:37:04 +0200

    Update main.py

--- v.2/main.py
+++ v.2/main.py
@@ -4,7 +4,7 @@ from game import Game
 
 class App:
     def __init__(self):
-        pyxel.init(160, 120, "meow")
+        pyxel.init(160, 120, "Flappy Bird")
         pyxel.load("my_resource.pyxres") # génère les tuyaux et l'oiseau
         self.game = Game()
         pyxel.run(self.update, self.draw)
