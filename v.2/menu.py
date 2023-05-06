0e262c27e1bffc208422a7ec8bb485142739eaa6
Julian <128595790+pasteque01@users.noreply.github.com>
2023-05-06 19:55:23 +0200

    Update menu.py

--- v.2/menu.py
+++ v.2/menu.py
@@ -7,6 +7,8 @@ class MainMenu:
         self.game = game
 
     def update(self):
+        if self.game.highscore < self.game.score:
+            self.game.highscore = self.game.score
         if pyxel.btnp(pyxel.KEY_RETURN):
             self.game.new_game()
 
@@ -15,8 +17,10 @@ class MainMenu:
         pyxel.text(45, 23, "___________________", 1)
         pyxel.text(60, 20, "Flappy Bird", 10)
         pyxel.text(30, 40, "Press ENTER to START", 7)
-        pyxel.text(30, 50, "Press ESC to QUIT", 7)
-        pyxel.text(30, 60, "Latest Score:", 7)
-        pyxel.text(90, 60, str(self.game.score), 9)
+        pyxel.text(30, 55, "Press ESC to QUIT", 7)
+        pyxel.text(30, 70, "LATEST Score:", 7)
+        pyxel.text(90, 70, str(self.game.score), 9)
+        pyxel.text(30, 85, "HIGHEST Score:", 7)
+        pyxel.text(90, 85, str(self.game.highscore), 9)
         if pyxel.btnp(pyxel.KEY_RETURN):
             self.game.draw()
